1b0ccbc07b3644268863c9f921510153869574e0
Iffath-Raza-chy <iffathraza@gmail.com>
2021-10-08 02:38:43 +0600

    SMTP SERVER

--- forms/contact.php
+++ forms/contact.php
@@ -1,8 +1,7 @@
 <?php
  
-
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'iffathraza@gmail.com';
+  $receiving_email_address = 'iffathraza.public@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -19,14 +18,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'in-v3.mailjet.com',
+    'username' => 'c51dcbd429b9457ff8b9b19257c6d84e',
+    'password' => '289c780291ab9a0f63eba4bcd106c8e2',
     'port' => '587'
   );
-  */
+ 
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
